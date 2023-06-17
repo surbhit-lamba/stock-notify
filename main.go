2be65673326e91ec60d4f6787c8e0a5107d540fb
Surbhit Personal <surbhitla@gmail.com>
2023-06-17 22:12:10 +0530

    first cron

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-co-op/gocron"
+)
 
 func main() {
-	fmt.Println("hello world new")
+	go runCronJobs()
+	fmt.Println("ye to ho gya")
+	time.Sleep(time.Minute)
+}
+
+func hello(name string) {
+	message := fmt.Sprintf("Hi, %v", name)
+	fmt.Println(message)
+}
+
+func runCronJobs() {
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Every(1).Minute().Do(func() {
+		hello("John Doe")
+	})
+
+	s.StartBlocking()
+}
+
+func setRoutes(r *gin.RouterGroup, ctx context.Context) {
+	r.GET("/health", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "web--healthy")
+	})
 }
