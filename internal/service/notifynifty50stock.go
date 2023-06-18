ce570cf1d628203d4a9f89b9b0429132c3be69a3
Surbhit Personal <surbhitla@gmail.com>
2023-06-18 21:39:07 +0530

    changes

--- internal/service/notifynifty50stock.go
+++ internal/service/notifynifty50stock.go
@@ -14,6 +14,7 @@ import (
 )
 
 func NotifyNifty50Stock(ctx context.Context) {
+	defer newrelic.StartSegmentWithContext(ctx, "NotifyNifty50Stock").End()
 	timeNow := time.Now()
 	l, err := time.LoadLocation("Asia/Kolkata")
 	if err != nil {
--- jobs/notifynifty50stock.go
+++ jobs/notifynifty50stock.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"context"
+	"fmt"
 	"stock-notify/internal/service"
 	"stock-notify/pkg/log"
 	"stock-notify/pkg/newrelic"
@@ -11,8 +12,12 @@ import (
 
 func NotifyNifty50Stock(ctx context.Context, s *gocron.Scheduler) {
 	_, err := s.Every(80).Minute().Do(func() {
-		defer newrelic.StartSegmentWithContext(ctx, "NotifyNifty50Stock").End()
-		service.NotifyNifty50Stock(ctx)
+		txn := newrelic.StartTransaction(fmt.Sprintf("stock-notify/NotifyNifty50Stock"), nil, nil)
+		defer func() {
+			txn.End()
+		}()
+		nrctx := newrelic.NewContext(context.Background(), txn)
+		service.NotifyNifty50Stock(nrctx)
 	})
 	if err != nil {
 		log.ErrorfWithContext(ctx, "Could not run NotifyNifty50Stock cron!!! Exiting")
