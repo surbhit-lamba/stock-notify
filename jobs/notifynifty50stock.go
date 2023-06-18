c1ae95832e9343275d600408922c7c339d5a2e7d
Surbhit Personal <surbhitla@gmail.com>
2023-06-18 12:59:07 +0530

    rem temp changes

--- jobs/notifynifty50stock.go
+++ jobs/notifynifty50stock.go
@@ -3,8 +3,10 @@ package jobs
 import (
 	"context"
 	"os"
+	"stock-notify/internal/constants"
 	"stock-notify/internal/utils"
 	"stock-notify/pkg/log"
+	"time"
 
 	"github.com/go-co-op/gocron"
 )
@@ -14,25 +16,18 @@ var toEmails = []string{os.Getenv("TO_EMAIL")}
 
 func NotifyNifty50Stock(ctx context.Context, s *gocron.Scheduler) {
 	_, err := s.Every(10).Seconds().Do(func() {
-		//weekDay := time.Now().Weekday()
-		//if utils.SliceContains(weekDay, constants.WeekDays) {
-		//	utils.SendEmailWithHTMLTemplate(
-		//		ctx,
-		//		fromEmail,
-		//		toEmails,
-		//		"emailtemplates/notifyNifty50Stock.html",
-		//		"[StockNotify] Stocks near 52 week low",
-		//	)
-		//} else {
-		//	log.ErrorfWithContext(ctx, "[NotifyNifty50Stock] Not a weekday, skipping sending mail")
-		//}
-		utils.SendEmailWithHTMLTemplate(
-			ctx,
-			fromEmail,
-			toEmails,
-			"emailtemplates/notifyNifty50Stock.html",
-			"[StockNotify] Stocks near 52 week low",
-		)
+		weekDay := time.Now().Weekday()
+		if utils.SliceContains(weekDay, constants.WeekDays) {
+			utils.SendEmailWithHTMLTemplate(
+				ctx,
+				fromEmail,
+				toEmails,
+				"emailtemplates/notifyNifty50Stock.html",
+				"[StockNotify] Stocks near 52 week low",
+			)
+		} else {
+			log.ErrorfWithContext(ctx, "[NotifyNifty50Stock] Not a weekday, skipping sending mail")
+		}
 	})
 	if err != nil {
 		log.ErrorfWithContext(ctx, "Could not run NotifyNifty50Stock cron!!! Exiting")
